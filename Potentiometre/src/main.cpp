dc8ab77429091f0095b0fe70a48494fecf08b400
Anne S <anne.sounanthanam@edu.devinci.fr>
2020-12-23 21:44:35 +0100

    Code de base potentiomètre

--- Potentiometre/src/main.cpp
+++ Potentiometre/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
+// PINS
+#define pot A0
+#define pwm 3
+
+// CAN resolution 10 bit
+#define CAN_min 0
+#define CAN_max 1023
+
+// PWM values
+#define pwm_min 0
+#define pwm_max 255
+
+
+int commande;
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // Pins setup
+  pinMode(pot, INPUT);
+  pinMode(pwm,OUTPUT);
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  commande=map(analogRead(pot), CAN_min, CAN_max, pwm_min, pwm_max);
+  analogWrite(pwm, commande); 
+
 }
